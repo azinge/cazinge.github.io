ca9b3b1fa28a346c5597a46e51fa8fc63cc116fe
cazinge <eddie.azinge@gmail.com>
2017-01-27 23:53:54 -0800

    updating title

--- src/components/App.js
+++ src/components/App.js
@@ -8,10 +8,9 @@ function App({ children }) {
   return (
     <div>
       <nav className="light-blue lighten-1" role="navigation">
-        <div className="nav-wrapper container"><a id="logo-container" href="#logo" className="brand-logo">Project Name</a>
+        <div className="nav-wrapper container"><a id="logo-container" href="#logo" className="brand-logo">Eddie Azinge</a>
           <ul className="right hide-on-med-and-down">
             <li><a href="#github">Github</a></li>
-            <li><a href="#version">v0.0.0</a></li>
           </ul>
         </div>
       </nav>
